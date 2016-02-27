ea3c2686418b781bfd8a2cd181f1f7865bbdee98
Levi <levi@teklife.info>
2016-02-27 12:09:57 -0500

    Fixing autoload to work with this repo.

--- autoload.php
+++ autoload.php
@@ -3,8 +3,8 @@ date_default_timezone_set('America/New_York');
 define("DS", DIRECTORY_SEPARATOR);
 define("ROOT", dirname(__FILE__));
 function autoloadClass($class) {
-    $class = str_replace("levidurfee\StockData", "", $class);
-    $classFile = ROOT . DS . 'src' . $class . '.php';
+    $class = str_replace("levidurfee\StockData\\", "", $class);
+    $classFile = ROOT . DS . 'src' . DS . $class . '.php';
     if(is_readable($classFile)) {
         require_once($classFile);
     }
