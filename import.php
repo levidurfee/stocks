9f90d6b8862bf592f830328a397ce6e33d39e530
Levi <levi@teklife.info>
2016-02-27 12:10:20 -0500

    Starting the logic for the Import class.

---
+++ import.php
@@ -0,0 +1,5 @@
+<?php
+require_once('autoload.php');
+$i = new \levidurfee\StockData\Import();
+
+$i->getFiles();
--- src/Import.php
+++ src/Import.php
@@ -1,7 +1,16 @@
 <?php namespace levidurfee\StockData;
 
-class Import extends levidurfee\StockData\StockData {
+class Import extends StockData {
+    protected $files = [];
+    
     public function __construct() {
         parent::__construct();
+    }
+    
+    /**
+     * Get a list of all the csv files that need to be imported
+     */
+    public function getFiles() {
+        echo ROOT . DS . 'data' . DS . 'csv' . DS;
     }
 }
